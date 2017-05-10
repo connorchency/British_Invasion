393c4efc8ef91c88c4f7d418c404e1385c2f5a6e
connorchency <connorchency@gmail.com>
2017-05-10 16:50:02 -0400

    update

--- Data/Preprocessed_Update.R
+++ Data/Preprocessed_Update.R
@@ -3,6 +3,7 @@ load('~/Documents/NYU/Text-as-Data/British_Invasion/Data/preprocessed.RData')
 
 BA = BA[,-1]
 BA = BA[!duplicated(BA[,1:2]),]
+BA = BA[which(BA$Lyrics != ""),]
 
 stopw = c('yeah','na','la','ba','da','sha', 
           'du','oo','rah','huh','ya','whoa',
@@ -15,16 +16,16 @@ lyrics_corpus = corpus(BA, docnames = "Song",text_field = "Lyrics")
  # Add More
 
 ly_BA = dfm(lyrics_corpus, stem=TRUE,
-              remove = c(stopwords("French"), stopw),
-              removePunct = T, removeNumbers = T,
-              removeSymbols = T, removeHyphens = T
-)
+            remove = c(stopwords("French"), stopw),
+            removePunct = T, removeNumbers = T,
+            removeSymbols = T, removeHyphens = T
+            )
 
 ly_BA_more = dfm(lyrics_corpus, stem=TRUE,
             remove = c(stopwords("French"), stopwords("English"), stopw),
             removePunct = T, removeNumbers = T,
             removeSymbols = T, removeHyphens = T
-)
+            )
 
 ly_BA_a = dfm(lyrics_corpus, stem=TRUE, groups = "Artist",
               remove = c(stopwords("French"), stopw),

ca4523c411f97babc2a590513189d39977f0e18e
connorchency <connorchency@gmail.com>
2017-05-10 20:26:05 -0400

    update

--- Data/Preprocessed_Update.R
+++ Data/Preprocessed_Update.R
@@ -11,48 +11,48 @@ BA = BA[which(lan != "french"),]
 
 stopw = c('yeah','na','la','ba','da','sha', 
           'du','oo','rah','huh','ya','whoa',
-          'oh', 'ooh','mmm','hmm','hm',
-          'nah')
+          'oh', 'ooh','mmm','hmm','hm', 'mm',
+          'nah', 'ah', 'aha', 'ohhh')
 
 lyrics_corpus = corpus(BA, docnames = "Song", text_field = "Lyrics")
 
-cus_stopwords = stopwords("English")[-(1:21)]
+cus_stopwords = stopwords("english")[-(1:21)]
 
 # Add More
 
 ly_BA = dfm(lyrics_corpus, stem=TRUE,
-            remove = c(stopwords("French"), stopw),
+            remove = c(stopwords("french"), stopw),
             removePunct = T, removeNumbers = T,
             removeSymbols = T, removeHyphens = T
             )
 
 ly_BA_more = dfm(lyrics_corpus, stem=TRUE,
-            remove = c(stopwords("French"), stopwords("English"), stopw),
+            remove = c(stopwords("french"), stopwords("english"), stopw),
             removePunct = T, removeNumbers = T,
             removeSymbols = T, removeHyphens = T
             )
 
 ly_BA_more_2 = dfm(lyrics_corpus, stem=TRUE,
                  remove = 
-                 c(stopwords("French"), cus_stopwords, stopw),
+                 c(stopwords("french"), cus_stopwords, stopw),
                  removePunct = T, removeNumbers = T,
                  removeSymbols = T, removeHyphens = T
 )
 
 ly_BA_a = dfm(lyrics_corpus, stem=TRUE, groups = "Artist",
-              remove = c(stopwords("French"), stopw),
+              remove = c(stopwords("french"), stopw),
               removePunct = T, removeNumbers = T,
               removeSymbols = T, removeHyphens = T
               )
 
 ly_BA_a_more = dfm(lyrics_corpus, stem=TRUE, groups = "Artist",
-                 remove = c(stopwords("French"), stopwords("English"), stopw),
+                 remove = c(stopwords("french"), stopwords("english"), stopw),
                  removePunct = T, removeNumbers = T,
                  removeSymbols = T, removeHyphens = T
 )
 
 ly_BA_a_more_2 = dfm(lyrics_corpus, stem=TRUE, groups = "Artist",
-                   remove = c(stopwords("French"), cus_stopwords, stopw),
+                   remove = c(stopwords("french"), cus_stopwords, stopw),
                    removePunct = T, removeNumbers = T,
                    removeSymbols = T, removeHyphens = T
 )
